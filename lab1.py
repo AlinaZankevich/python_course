0c3ea797c3cacce53a8e68020647e92da2c290c9
Alina Zankevich <Alina@alexeys-mbp.myfiosgateway.com>
2021-01-19 20:03:20 -0500

    updated task

--- lab1.py
+++ lab1.py
@@ -13,22 +13,22 @@ def task1():
     print(neg_count)
 
 
-def ticket_is_lucky(ticket):
-    sum_of_num = 0
-    for num in str(ticket):
-        sum_of_num += int(num)
-    if sum_of_num % 7 == 0:
-        return True
-    else:
-        return False
-
-
 def task2():
+    def ticket_is_lucky(ticket):
+        sum_of_num = 0
+        for num in str(ticket):
+            sum_of_num += int(num)
+        if sum_of_num % 7 == 0:
+            return True
+        else:
+            return False
+
     first_ticket = 100000
     last_ticket = 999999
     for ticket in range(first_ticket, last_ticket + 1):
         if ticket_is_lucky(ticket) and ticket_is_lucky(ticket + 1):
             print(f'{ticket} and {ticket + 1} are lucky')
+
 
 task2()
 
